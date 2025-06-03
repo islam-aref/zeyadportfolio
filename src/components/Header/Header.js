4a198d1f17a755d036988c437d0ad85953c42256
Islam Aref <125500794+islam-aref@users.noreply.github.com>
2025-06-04 01:03:34 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,29 +12,7 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.logo}>Zeyad Hisham Portfolio</div>
 
-      {/* Mobile Hamburger Menu */}
-      <button
-        className={styles.menuButton}
-        onClick={toggleMenu}
-        aria-label="Toggle menu"
-      >
-        <span className={styles.menuIcon}></span>
-      </button>
 
-      {/* Desktop & Mobile Navigation */}
-      <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ""}`}>
-        <ul>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#footer">Contact</a>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
